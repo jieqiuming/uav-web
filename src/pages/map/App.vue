df301510509bf964023b311db898e192d684e0de
_truemen <779351528@qq.com>
2026-01-26 15:52:55 +0800

    feat: Add map page and its initial 3D scene configuration.

--- src/pages/map/App.vue
+++ src/pages/map/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <main-view @mapLoaded="mapLoaded" :url="sceneUrl" />
+  <a-config-provider :theme="{ algorithm: theme.darkAlgorithm }">
+    <main-view @mapLoaded="mapLoaded" :url="sceneUrl" />
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue"
+import { theme, ConfigProvider } from "ant-design-vue"
 import MainView from "@mars/components/mars-work/main-view.vue"
 import { getQueryString } from "@mars/utils/mars-util"
 
